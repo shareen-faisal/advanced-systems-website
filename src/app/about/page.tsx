7000cf1d1300b4f7b22b2a4ffa6301d82660d90d
Eman-333 <221400020@gift.edu.pk>
2025-10-10 19:04:34 +0500

    about styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,61 +1,10 @@
-// import Image from "next/image";
-// import styles from "./About.module.css";
-// import { FaUtensils, FaStar } from "react-icons/fa";
+import About from "@/components/About";
+import React from "react";
 
-// export default function AboutUs() {
-//   return (
-//     <section className={styles.about}>
-//       {/* Left side - Image */}
-//       <div className={styles.imageWrapper}>
-//         <Image
-//           src="/burger.png" // place your burger image in public/burger.png
-//           alt="Delicious Burger"
-//           width={500}
-//           height={400}
-//           className={styles.image}
-//         />
-//       </div>
+const AboutUs: React.FC = () => {
+  return (
+    <About />
+  );
+};
 
-//       {/* Right side - Content */}
-//       <div className={styles.content}>
-//         <h4 className={styles.subtitle}>About Our Food</h4>
-//         <h2 className={styles.title}>
-//           Where Quality Meet Excellent <span>Service.</span>
-//         </h2>
-//         <p className={styles.description}>
-//           It’s the perfect dining experience where every dish is crafted with
-//           fresh, high-quality ingredients. Experience quick and efficient
-//           service that ensures your food is served fresh.
-//         </p>
-
-//         <div className={styles.features}>
-//           <div className={styles.feature}>
-//             <FaUtensils className={styles.icon} />
-//             <div>
-//               <h5>Super Quality Food</h5>
-//               <p>
-//                 A team of dreamers and doers building unique interactive music
-//                 and art.
-//               </p>
-//             </div>
-//           </div>
-
-//           <div className={styles.feature}>
-//             <FaStar className={styles.icon} />
-//             <div>
-//               <h5>Well Reputation</h5>
-//               <p>
-//                 A team of dreamers and doers building unique interactive music
-//                 and art.
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-
-//         <h5 className={styles.footer}>
-//           Flavour Town <span>CUSTOMER’S EXPERIENCE IS OUR HIGHEST PRIORITY.</span>
-//         </h5>
-//       </div>
-//     </section>
-//   );
-// }
+export default AboutUs;
